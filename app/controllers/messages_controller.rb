344ca5e7b2bdb6a91f28ac9f23f04ba76bdef428
hannaoyo <hannaoyo11@gmail.com>
2024-02-27 23:04:41 +0900

    ユーザー認証機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  # ログアウト状態の場合でもメイン画面やチャット画面が表示される、それ以外はログインページへ遷移
+  before_action :authenticate_user!, except: [:index, :show]
   def index
   end
 end
