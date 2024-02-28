9a86eff91692b3adc555eda72dcad5ead6291023
hannaoyo <hannaoyo11@gmail.com>
2024-02-28 12:10:16 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "trains#index"
-  resources :trains do
-    resources :users, only: [:edit, :update]
-  end
+  resources :users, only: [:edit, :update]
+  resources :trains, only: [:new, :create]
 end
