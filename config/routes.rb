529e40b4bec6e4db4c2d237671b8dab7b4623db7
hannaoyo <hannaoyo11@gmail.com>
2024-03-04 16:25:46 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "trains#index"
   resources :users, only: [:edit, :update]
-  resources :trains, only: [:new, :create]
+  resources :trains, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
